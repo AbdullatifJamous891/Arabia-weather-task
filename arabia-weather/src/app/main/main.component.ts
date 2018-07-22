9393ba5e28e9ed202fbaf9f5aa52b41c25df2e7d
AbdullatifJamous891 <Abdullatif.jamous891@gmail.com>
2018-07-23 01:46:04 +0300

    fix style

--- arabia-weather/src/app/main/main.component.ts
+++ arabia-weather/src/app/main/main.component.ts
@@ -6,8 +6,8 @@ import axios from 'axios';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
-  alo='weather'
-  url = `https://api.giphy.com/v1/gifs/search?api_key=UsrbTXZTtwKxOd0uMFMT9MYF5e8EY99E&q=${this.alo}`
+  tag='weather'
+  url = `https://api.giphy.com/v1/gifs/search?api_key=UsrbTXZTtwKxOd0uMFMT9MYF5e8EY99E&q=${this.tag}`
   Imagearray = [];
   
   constructor() { }
